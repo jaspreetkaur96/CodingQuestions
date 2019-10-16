a3644b9730816cb9e9846350bee7ba382f2be8e2
Chirag <42274144+Chirag161198@users.noreply.github.com>
2019-10-16 19:15:16 +0530

    Update power.cpp
    Made code readable (indented code), user-friendly (ask user for specific inputs) and added comments.

--- power.cpp
+++ power.cpp
@@ -4,37 +4,47 @@ using namespace std;
 int power(int x, int n);
 int spower(int x, int n);
 
-int main(int argc, char** argv){
+int main(){
+    int x,n,choice;
+    
+    // Take input from user
+    cout<<"Enter base and power (separated by space): ";
+    cin>>x>>n;
 
-
- cout<<spower(3, 6);
-
+    // Take choice from user
+    cout<<"Press 1 for power(x,n) and 2 for spower(x,n): ";
+    cin>>choice;
+    
+    switch(choice){
+        case 1: cout<<power(x,n);
+                break;
+        case 2: cout<<spower(x,n);
+                break;
+        default: break;
+    }
 }
 
+/*
+    power
+    -----
+    calculating x^n using recursion
+*/
 int power(int x,int n){
-
-if(n==0){
-    return 1;
-}
-
- int ans = x * power( x, n-1);
- return ans;
-
+    if(n==0){
+        return 1;
+    }
+    return x * power(x,n-1);
 }
 
 int spower(int x,int n){
+    if(n==0){
+        return 1;
+    }
+    int sp = spower(x, n/2);
+    int ans = sp * sp;
 
-if(n==0){
-    return 1;
+    if(n%2==1){
+        ans = ans * x;
+    }
+    return ans;
 }
-
- int sp = spower(x, n/2);
- int ans = sp * sp;
-
- if(n%2==1){
-    ans = ans * x;
- }
-
- return ans;
-}
-
